039fb1ee00e240fdcb8b2308c1946ccb3b419c80
sonu kumar <130893865+sonukumar4546@users.noreply.github.com>
2024-05-22 13:36:52 -0700

    Update 1 d array  cout hd.cpp.cpp
    some change

--- 1 d array  cout hd.cpp.cpp
+++ 1 d array  cout hd.cpp.cpp
@@ -1,17 +1,9 @@
 
-
-
-
-
-
 # include <iostream>
-
 using namespace std ;
 
 
 int main()
-
-
 {
      
      int A[5];
@@ -24,9 +16,6 @@ int main()
       
       cin>>A[i];
 
-
-
-
      }
       
       
@@ -37,13 +26,7 @@ int main()
      {
 
       cout<<A[i]<<"  ";
-      
- 
- 
-
-
-
-
+     
      }
 
 
